0c795c47a4c1e52f5ae1df10bd78717ec355d23e
JoakoSavini <savinijoaquin@gmail.com>
2024-07-04 19:49:30 -0300

    Casi listo el js del formulario

---
+++ materialize/js/contacto.js
@@ -0,0 +1,45 @@
+//Obtengo la informacion del DOM
+document.getElementById('datos_form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const nombre = document.querySelector('#first_name');
+    const apellido = document.querySelector('#last_name');
+    const email = document.querySelector('#email');
+    const mensaje = document.querySelector('#message');
+
+    //*Si se valida, creo nuevo usuario
+    if (validarDatos(nombre, apellido, email, mensaje)){
+        const nuevo_usuario = {
+            id: generarID(),
+            nombre,
+            apellido,
+            email,
+            mensaje
+        };
+        let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+        usuarios.push(nuevo_usuario);
+        localStorage.setItem('usuarios', JSON.stringify(usuarios));
+        alert('Sus datos han sido enviados correctamente')
+        document.getElementById('datos_form').reset();
+    }
+})
+
+
+//Declaro las funciones de toma de valores
+
+//*Primero la funcion de validación
+const validarDatos = (nombre, apellido, email, mensaje) => {
+    if (!nombre || !apellido || !email || !mensaje){
+        alert('Complete todos los campos')
+        return false;
+    }
+}
+
+//*Para crear ID's
+const generarID = () => {
+    let id = localStorage.getItem('idUsuario') || '0';
+    id = parseInt(id) + 1;
+    localStorage.setItem('idUsuario', id.toString());
+    return id;
+}
+
